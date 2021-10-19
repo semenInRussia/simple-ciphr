4ecd8f920ae49589bcfa66cf138cbc9e6bcc733b
Semen <51054743+semenInRussia@users.noreply.github.com>
2021-10-19 18:37:43 +0300

    Update tabar.js

--- tabar.js
+++ tabar.js
@@ -1,25 +1,54 @@
 let vowelsChars = Array.from('eyuioaуехыаоэяиюEYUIOAУЕХЫАОЭЯИЮ')
 
 function encode(string, syllable) {
-  return Array.from(string)
-    .map(char => _addSyllableIfIsVowel(char, syllable))
-    .reduce((previousChar, currentChar) => (previousChar + currentChar), "")
+    let result = ''
+
+    let indexesOfVowels = _getIndexesOfVowels(string)
+    let lastCharIndex = 0;
+
+    for (indexOfVowel of indexesOfVowels) {
+        let encodedPieceOfString = getPieceOfStringAndEncode({
+            string, syllable, indexOfVowel, lastCharIndex
+        })
+
+        result += encodedPieceOfString
+        lastCharIndex = indexOfVowel
+    }
+
+    let lastPieceOfString = string.slice(lastCharIndex, string.length)
+
+    result += lastPieceOfString
+
+    return result
 }
 
-function _addSyllableIfIsVowel(char, syllable) {
-  if (vowelsChars.includes(char)) {
-    return char + syllable
-  }
-  
-  else {
-    return char
-  }
+function getPieceOfStringAndEncode({string, syllable, indexOfVowel, lastCharIndex}) {
+    let pieceOfString = string.slice(lastCharIndex, indexOfVowel + 1)
+    let encodedPieceOfString = pieceOfString + syllable
+
+    return encodedPieceOfString    
+}
+
+function _getIndexesOfVowels(string) {
+    return Array.from(string)
+        .map((value, index) => index)
+        .filter(index => _isVowelChar(string[index]))
 }
 
 function decode(string, syllable) {
-  let syllableLength = syllable.length
-  
-  return string
-    .split(syllable)
-    .reduce((previousChar, currentChar) => (previousChar + currentChar), "")
+    let piecesOfString = string.split(syllable)
+    let piecesOfStringWithExtraVowel = _deleteFirstElement(piecesOfString)
+    let decodedPiecesOfString = _deleteFirstElementFromInnerArrays(piecesOfStringWithExtraVowel)
+    let decoded = firstElement(piecesOfString) + sum(decodedPiecesOfString)
+
+    return decoded
 }
+
+let _deleteFirstElementFromInnerArrays = arr => arr.map(_deleteFirstElement)
+let _deleteFirstElement = arr => (arr.slice(1, arr.length))
+
+let _isVowelChar = char => vowelsChars.includes(char)
+
+let lastElement = array => array[array.length - 1]
+let firstElement = array => array[0]
+let sum = arr => arr.reduce((previous, current) => previous + current)
